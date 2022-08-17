6e7e279f59cc2b1b98d6664489082cb5280bbc6a
Tilak Dave <davetilak003@gmail.com>
2022-08-17 17:53:03 +0530

    Setting up Firestore, Chakra UI, Absolute Import & alises

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,38 @@
-import { ProvideAuth } from '../lib/auth'
-import '../styles/globals.css'
+import { ChakraProvider, CSSReset } from '@chakra-ui/react'
+import { Global, css } from '@emotion/react'
 
-function MyApp({ Component, pageProps }) {
-  return <ProvideAuth>
-    <Component {...pageProps} />
-  </ProvideAuth>
+import { AuthProvider } from '@/lib/auth'
+import customTheme from '@/styles/theme'
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            min-width: 360px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
+
+function App({ Component, pageProps }) {
+  return <ChakraProvider theme={customTheme}>
+    <GlobalStyle />
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  </ChakraProvider>
 }
 
-export default MyApp
+export default App
