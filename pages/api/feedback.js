251a11ef67b7c9b753d7eef339529eaa1f800b98
Tilak Dave <davetilak003@gmail.com>
2022-08-22 16:02:47 +0530

    Add pino logging with logflare on API routs (#10)
    * Add pino logging with logflare on API routs
    
    * Node env

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,6 +1,8 @@
 import { auth } from '@/lib/firebase-admin'
 import { getuserFeedback } from "@/lib/db-admin";
 
+import { logger, formatObjectKeys } from '@/utils/logger';
+
 export default async function handler(req, res) {
     try {
         const { uid } = await auth.verifyIdToken(req.headers.token);
@@ -8,6 +10,20 @@ export default async function handler(req, res) {
 
         res.status(200).json({ feedback });
     } catch (error) {
+        logger.error(
+            {
+              request: {
+                headers: formatObjectKeys(req.headers),
+                url: req.url,
+                method: req.method
+              },
+              response: {
+                statusCode: res.statusCode
+              }
+            },
+            error.message
+          );
+
         res.status(500).json({ error });
     }
 }
--- pages/api/sites.js
+++ pages/api/sites.js
@@ -1,6 +1,8 @@
 import { auth } from '@/lib/firebase-admin'
 import { getUserSites } from "@/lib/db-admin";
 
+import { logger, formatObjectKeys } from '@/utils/logger';
+
 export default async function handler(req, res) {
   try {
     const { uid } = await auth.verifyIdToken(req.headers.token);
@@ -8,6 +10,21 @@ export default async function handler(req, res) {
 
     res.status(200).json({ sites });
   } catch (error) {
+
+    logger.error(
+      {
+        request: {
+          headers: formatObjectKeys(req.headers),
+          url: req.url,
+          method: req.method
+        },
+        response: {
+          statusCode: res.statusCode
+        }
+      },
+      error.message
+    );
+
     res.status(500).json({ error });
   }
 }
